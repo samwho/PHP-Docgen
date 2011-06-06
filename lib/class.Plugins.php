6cc6292428c4bcffcdb33ac35e8235a0942aae8f
Sam Rose <samwho@lbak.co.uk>
2011-06-06 18:06:15 +0100

    Made the Plugins class a bit more reflective and useful.

--- lib/class.Plugins.php
+++ lib/class.Plugins.php
@@ -1,13 +1,56 @@
 <?php
 
 class Plugins {
+    private static $loaded_plugins = array();
+
+    /**
+     * Returns the directory that docgen looks for plugins in.
+     */
+    public static function directory() {
+        return dirname(__FILE__) . '/../plugin/';
+    }
 
     /**
      * Loads all files ending in .php in the plugins directory.
      */
     public static function loadAll() {
-        foreach(glob(dirname(__FILE__) . '/../plugin/*.php') as $file) {
-            require_once $file;
+        foreach(glob(self::directory() . '*.php') as $file) {
+            if (!in_array($file, get_included_files()) {
+                require $file;
+                self::$loaded_plugins[] = $file;
+            }
         }
+    }
+
+    /**
+     * Loads a plugin by its file name. This function just requires a file by
+     * prepending the plugin directory and appending .php to the argument
+     * passed in.
+     *
+     * Example:
+     *
+     * $plugin_dir . $file . '.php'
+     *
+     * The file name does not need a leading forward slash.
+     *
+     * @param string $name Plugin file name without path or extension.
+     */
+    public static function load($name) {
+        $file = self::directory() . $name . '.php';
+
+        if (!in_array($file, get_included_files()) {
+            require $file;
+            self::$loaded_plugins[] = $file;
+        }
+    }
+
+    /**
+     * Returns an array of plugin file paths that have been loaded into the
+     * application.
+     *
+     * @return array $loaded_plugins File paths of plugins that have been loaded.
+     */
+    public static function getLoadedPlugins() {
+        return self::$loaded_plugins;
     }
 }
